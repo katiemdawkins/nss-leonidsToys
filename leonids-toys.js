6649e8221a392c595adcdf7c196fde64bd1df5eb
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-11 14:55:02 -0600

    for.. of loop for string interpolation

--- leonids-toys.js
+++ leonids-toys.js
@@ -7,9 +7,9 @@ const toys = [
         price: 45
     },
     {
-        id: 1,
-        name: "Lego Castle Set",
-        manufacturer: "Lego",
+        id: 2,
+        name: "Malibu Barbie",
+        manufacturer: "Matel",
         numberInStock: 22,
         price: 45
     },
@@ -22,6 +22,36 @@ const toys = [
     }
 ]
 
-console.log(toys)
+const slime = {
+    id: 4,
+    name: "Slime- Green",
+    manufacturer: "Matel",
+    numberInStock: 16,
+    price: 11
+}
 
+const elmo = {
+    id: 5,
+    name: "Elmo Toy",
+    manufacturer: "Matel",
+    numberInStock: 13,
+    price: 25
+}
 
+toys.push(slime)
+toys.push(elmo)
+
+for (const toy of toys){
+    console.log(`The ${toy.name} is made by ${toy.manufacturer} and costs $${toy.price}`)
+}
+//console.log(toys) commented out to see just the for... of loop in the console
+
+//for (const toy of toys) {
+    //console.log(toy.name)
+//}
+
+// the for... of loop creates a new variable that goes inside the main variable
+// inside it grabs the property I want with dot notation.
+// example: for (const newVariable of oldVariable) {
+//              console.log(newVariable.property)
+//               }
