000f8ddeafd3db5ab7faf8121fb3f191cbc17459
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-11 13:59:34 -0600

    first commit with js

---
+++ leonids-toys.js
@@ -0,0 +1,27 @@
+const toys = [
+    {
+        id: 1,
+        name: "Lego Castle Set",
+        manufacturer: "Lego",
+        numberInStock: 22,
+        price: 45
+    },
+    {
+        id: 1,
+        name: "Lego Castle Set",
+        manufacturer: "Lego",
+        numberInStock: 22,
+        price: 45
+    },
+    {
+        id: 3,
+        name: "Fox Puzzle",
+        manufacturer: "Matel",
+        numberInStock: 3,
+        price: 14
+    }
+]
+
+console.log(toys)
+
+
